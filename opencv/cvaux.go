7ee82239880805987efd80618b55c60d11e3a469
SamZhou <sam@mixmedia.com>
2017-02-10 14:45:39 +0800

    修复window下编译问题

--- opencv/cvaux.go
+++ opencv/cvaux.go
@@ -8,7 +8,7 @@ package opencv
 //#cgo linux  pkg-config: opencv
 //#cgo darwin pkg-config: opencv
 //#cgo freebsd pkg-config: opencv
-//#cgo windows LDFLAGS: -lopencv_core242.dll -lopencv_imgproc242.dll -lopencv_photo242.dll -lopencv_highgui242.dll -lstdc++
+//#cgo windows LDFLAGS: -lopencv_core242.dll -lopencv_imgproc242.dll -lopencv_photo242.dll -lopencv_highgui242.dll -lopencv_objdetect242.dll -lstdc++
 import "C"
 import (
 	"unsafe"
